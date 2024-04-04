fba0c075684ff3db13e5d91d8f2fb2e5147fc27f
Oscar Gariel Naranjo <144272461+onaranjoeng@users.noreply.github.com>
2024-04-04 00:47:04 -0400

    Add files via upload

--- activity.php
+++ activity.php
@@ -11,7 +11,45 @@ $settings = $stmt->fetch(PDO::FETCH_ASSOC);
 // Function to get daily power consumption for the last 7 days
 function getWeeklyPowerData() {
     global $dbConnection;
-    $stmt = $dbConnection->query("SELECT strftime('%m-%d-%Y', datetime) AS day, SUM(real_power_CT1) + SUM(real_power_CT2) AS total_power FROM power_data GROUP BY day ORDER BY day DESC LIMIT 7");
+    $stmt = $dbConnection->query("WITH EnergyData AS (
+        SELECT
+            datetime,
+            (julianday(datetime) - julianday(LAG(datetime) OVER (ORDER BY datetime))) AS seconds_CT1,
+            (julianday(datetime) - julianday(LAG(datetime) OVER (ORDER BY datetime))) AS seconds_CT2, 
+            CASE 
+                WHEN real_power_CT1 < 0 or real_power_CT1 like 'NaN' THEN 0
+                ELSE real_power_CT1
+            END AS energy_watts_CT1,
+            CASE 
+                WHEN real_power_CT2 < 0 or real_power_CT2 like 'NaN' THEN 0
+                ELSE real_power_CT2
+            END AS energy_watts_CT2
+        FROM
+            power_data
+        WHERE 
+            energy_watts_CT1 > 0
+    )
+    SELECT
+        CASE strftime('%w', datetime)
+            WHEN '0' THEN 'Sun'
+            WHEN '1' THEN 'Mon'
+            WHEN '2' THEN 'Tues'
+            WHEN '3' THEN 'Wed'
+            WHEN '4' THEN 'Thurs'
+            WHEN '5' THEN 'Fri'
+            WHEN '6' THEN 'Sat'
+        END || ', ' || strftime('%m-%d', datetime) AS day,
+        (SUM(CASE WHEN seconds_CT1 = 0 or seconds_CT1 is null THEN 1 ELSE seconds_CT1 END * energy_watts_CT1)/SUM(CASE WHEN energy_watts_CT1 = 0 THEN 0 ELSE CASE WHEN seconds_CT1 = 0 or seconds_CT1 is null THEN 1 ELSE seconds_CT1 END END)) *24 / 1000 AS total_power,
+        (SUM(CASE WHEN seconds_CT2 = 0 or seconds_CT2 is null THEN 1 ELSE seconds_CT2 END * energy_watts_CT2)/SUM(CASE WHEN energy_watts_CT2 = 0 THEN 0 ELSE CASE WHEN seconds_CT2 = 0 or seconds_CT2 is null THEN 1 ELSE seconds_CT2 END END)) *24 / 1000 AS total_power2
+    FROM
+        EnergyData
+    WHERE
+        strftime('%W', datetime) >= strftime('%W', 'now') AND
+        strftime('%Y', datetime) = strftime('%Y', 'now')
+    GROUP BY
+        DATE(datetime)
+    ORDER BY strftime('%w', datetime) DESC LIMIT 7");
+
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
     return array_reverse($result); // Reverse the array to start from the oldest day
 }
@@ -48,31 +86,48 @@ $weeklyPowerData = getWeeklyPowerData();
     </footer>
 
     <div class="chart-container">
-        <h2>Weekly Power Consumption</h2>
-        <canvas id="weeklyChart"></canvas>
-        <h2>Weekly Costs</h2>
-        <canvas id="costChart"></canvas>
+        <h2><?php echo ($settings['ct1_label']); ?> Daily Consumption</h2>
+        <canvas id="dailyChartCT1"></canvas>
+        <h2><?php echo ($settings['ct1_label']); ?> Weekly Costs</h2>
+        <canvas id="costChartCT1"></canvas>
+        <h2><?php echo ($settings['ct2_label']); ?> Daily Consumption</h2>
+        <canvas id="dailyChartCT2"></canvas>
+        <h2><?php echo ($settings['ct2_label']); ?> Weekly Costs</h2>
+        <canvas id="costChartCT2"></canvas>
     </div>
 
-    
-
     <script>
+        // GENERAL WEEKLY DATA
         var weeklyPowerData = <?php echo json_encode($weeklyPowerData); ?>;
-        var labels = weeklyPowerData.map(function(data) { return data.day; });
-        var powerValues = weeklyPowerData.map(function(data) { return (data.total_power / 1000).toFixed(2); }); // Convert watts to kWh
-        var costValues = powerValues.map(function(power) { return (power * <?php echo $settings['KiloWatt_US']; ?>).toFixed(2); }); // Calculate costs
+        var labels = weeklyPowerData.map(function(data) { 
+            return data.day; });
+        // FOR BLUE AKA CT1 
+        var powerValuesCT1 = weeklyPowerData.map(function(data) { 
+            return (data.total_power).toFixed(4); }); // Convert watts to kWh
+        var costValuesCT1 = powerValuesCT1.map(function(power) { 
+            return (power * <?php echo $settings['KiloWatt_US']; ?>).toFixed(2); }); // Calculate costs
+        var totalCostCT1 = costValuesCT1.reduce(function(acc, curr){
+            return acc + parseFloat(curr);}, 0); // Calculate the total cost
 
-        // Weekly Power Consumption Chart
-        var weeklyChart = new Chart(document.getElementById('weeklyChart'), {
+        // FOR ORANGE AKA CT2
+        var powerValuesCT2 = weeklyPowerData.map(function(data) { 
+            return (data.total_power).toFixed(4); }); // Convert watts to kWh
+        var costValuesCT2 = powerValuesCT2.map(function(power) { 
+            return (power * <?php echo $settings['KiloWatt_US']; ?>).toFixed(2); }); // Calculate costs
+        var totalCostCT2 = costValuesCT2.reduce(function(acc, curr){
+            return acc + parseFloat(curr);}, 0); // Calculate the total cost
+
+        // Daily Power Consumption Chart CT1
+        var dailyChartCT1 = new Chart(document.getElementById('dailyChartCT1'), {
             type: 'bar',
             data: {
                 labels: labels,
                 datasets: [{
                     label: 'Total Power (kWh)',
-                    data: powerValues,
-                    backgroundColor: 'rgba(54, 162, 235, 1)',
+                    data: powerValuesCT1,
+                    backgroundColor: 'rgba(54, 162, 235, .9)',
                     borderColor: 'rgba(54, 162, 235, 1)',
-                    borderWidth: 1
+                    borderWidth: 2
                 }]
             },
             options: {
@@ -97,36 +152,183 @@ $weeklyPowerData = getWeeklyPowerData();
             }
         });
 
-        // Weekly Costs Chart
-        var costChart = new Chart(document.getElementById('costChart'), {
+        // Doughnut Chart for Costs CT1
+        var costChartCT1 = new Chart(document.getElementById('costChartCT1'), {
             type: 'doughnut',
+            data: {
+                labels: labels, // Add the total cost label and day labels
+                datasets: [{
+                    label: 'Costs ($)',
+                    data: costValuesCT1, // Add the total cost and day costs
+                    backgroundColor: labels.map(() => 'rgba(54, 162, 235, .9)'), // Color for day costs
+                    borderColor: labels.map(() => 'rgba(220,220,220)'), // Border color for day costs
+                    borderWidth: 2
+                }]
+            },
+            options: {
+                layout: {
+                    padding: {
+                        bottom: 25                    }
+                },
+                plugins: {
+                    tooltip: {
+                        callbacks: {
+                            label: function(context) {
+                                var label = context.label || '';
+                                if (label) {
+                                    label += ': $';
+                                }
+                                if (context.parsed !== undefined) {
+                                    label += context.parsed;
+                                }
+                                return label;
+                            }
+                        }
+                    },
+                    legend: {
+                        display: false // Hide the legend since we are displaying the total cost in the center
+                    },
+                    doughnutlabel: {
+                        labels: [{
+                            text: 'Total: $' + totalCostCT1.toFixed(2),
+                            font: {
+                                size: '20'
+                            }
+                        }]
+                    }
+                }
+            },
+            plugins: [{
+                beforeDraw: function(chart) {
+                    var width = chart.width,
+                        height = chart.height,
+                        ctx = chart.ctx;
+                    ctx.restore();
+                    var fontSize = (height / 300).toFixed(2);
+                    ctx.font = fontSize + "em sans-serif";
+                    ctx.textBaseline = "middle";
+                    var totalCostText = "Total Cost:",
+                        totalCostTextX = Math.round((width - ctx.measureText(totalCostText).width) / 2),
+                        totalCostTextY = height / 2.1 - (fontSize * 10); // Position "Total Cost" above the amount
+                    ctx.fillText(totalCostText, totalCostTextX, totalCostTextY);
+
+                    var amountFontSize = (height / 300).toFixed(2); // Adjust font size for the amount
+                    ctx.font = amountFontSize + "em sans-serif";
+                    var amountText = "$" + totalCostCT1.toFixed(2),
+                        amountTextX = Math.round((width - ctx.measureText(amountText).width) / 2),
+                        amountTextY = height / 2.05 + (amountFontSize * 8); // Position the amount below "Total Cost"
+                    ctx.fillText(amountText, amountTextX, amountTextY);
+                    ctx.save();
+                }
+            }]
+        });
+
+         // Daily Power Consumption Chart CT2
+         var dailyChartCT2 = new Chart(document.getElementById('dailyChartCT2'), {
+            type: 'bar',
             data: {
                 labels: labels,
                 datasets: [{
-                    label: 'Costs ($)',
-                    data: costValues,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)',
-                        'rgba(201, 203, 207, 1)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)',
-                        'rgba(201, 203, 207, 1)'
-                    ],
-                    borderWidth: 5
+                    label: 'Total Power (kWh)',
+                    data: powerValuesCT2,
+                    backgroundColor: 'rgba(255,165,1,1)',
+                    borderColor: 'rgba(255,165,1,1)',
+                    borderWidth: 2
                 }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        title: {
+                            display: true,
+                            text: 'kWh'
+                        }
+                    }
+                },
+                plugins: {
+                    legend: {
+                        labels: {
+                            font: {
+                                size: 15 // Set the font size for the dataset labels
+                            }
+                        }
+                    }
+                }
             }
         });
+
+        // Doughnut Chart for Costs CT2
+        var costChartCT2 = new Chart(document.getElementById('costChartCT2'), {
+            type: 'doughnut',
+            data: {
+                labels: labels, // Add the total cost label and day labels
+                datasets: [{
+                    label: 'Costs ($)',
+                    data: costValuesCT2, // Add the total cost and day costs
+                    backgroundColor: labels.map(() => 'rgba(255,165,1,1)'), // Color for day costs
+                    borderColor: labels.map(() => 'rgba(220,220,220)'), // Border color for day costs
+                    borderWidth: 2
+                }]
+            },
+            options: {
+                layout: {
+                    padding: {
+                        bottom: 25                    }
+                },
+                plugins: {
+                    tooltip: {
+                        callbacks: {
+                            label: function(context) {
+                                var label = context.label || '';
+                                if (label) {
+                                    label += ': $';
+                                }
+                                if (context.parsed !== undefined) {
+                                    label += context.parsed;
+                                }
+                                return label;
+                            }
+                        }
+                    },
+                    legend: {
+                        display: false // Hide the legend since we are displaying the total cost in the center
+                    },
+                    doughnutlabel: {
+                        labels: [{
+                            text: 'Total: $' + totalCostCT2.toFixed(2),
+                            font: {
+                                size: '20'
+                            }
+                        }]
+                    }
+                }
+            },
+            plugins: [{
+                beforeDraw: function(chart) {
+                    var width = chart.width,
+                        height = chart.height,
+                        ctx = chart.ctx;
+                    ctx.restore();
+                    var fontSize = (height / 300).toFixed(2);
+                    ctx.font = fontSize + "em sans-serif";
+                    ctx.textBaseline = "middle";
+                    var totalCostText = "Total Cost:",
+                        totalCostTextX = Math.round((width - ctx.measureText(totalCostText).width) / 2),
+                        totalCostTextY = height / 2.1 - (fontSize * 10); // Position "Total Cost" above the amount
+                    ctx.fillText(totalCostText, totalCostTextX, totalCostTextY);
+
+                    var amountFontSize = (height / 300).toFixed(2); // Adjust font size for the amount
+                    ctx.font = amountFontSize + "em sans-serif";
+                    var amountText = "$" + totalCostCT2.toFixed(2),
+                        amountTextX = Math.round((width - ctx.measureText(amountText).width) / 2),
+                        amountTextY = height / 2.05 + (amountFontSize * 8); // Position the amount below "Total Cost"
+                    ctx.fillText(amountText, amountTextX, amountTextY);
+                    ctx.save();
+                }
+            }]
+        });
+
     </script>
 
 
